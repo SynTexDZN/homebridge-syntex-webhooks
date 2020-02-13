0722bb06e3c8b08caefcc78f9d7342e8a4cfab97
SynTexDZN <syntexdzn@t-online.de>
2020-02-13 22:25:44 +0100

    light tests

--- index.js
+++ index.js
@@ -585,6 +585,10 @@ SynTexWebHookStripeRGBAccessory.prototype.setHue = function(level, callback)
     g = hue2rgb(p, q, h);
     b = hue2rgb(p, q, h - 1/3);
 
+    log('R:', r);
+    log('G:', g);
+    log('B:', b);
+
     var theRequest = {
         method : "GET",
         url : "http://192.168.188.155/color?r=" + r + "&g=" + g + "&b=" + b,
