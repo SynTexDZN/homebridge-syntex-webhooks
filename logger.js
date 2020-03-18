d726901fa7197c46515715f29aa44f90cfdb8ffb
SynTexDZN <syntexdzn@t-online.de>
2020-03-18 16:51:16 +0100

    fixes

--- logger.js
+++ logger.js
@@ -24,27 +24,27 @@ logger.log = function(level, message)
 
         if(level == 'success')
         {
-            color = '[' + prefix + '] \x1b[32m%s\x1b[0m', "[SUCCESS]";
+            color = '[' + prefix + '] \x1b[32m%s\x1b[0m';
         }
         else if(level == 'update')
         {
-            color = '[' + prefix + '] \x1b[36m%s\x1b[0m', "[UPDATE]";
+            color = '[' + prefix + '] \x1b[36m%s\x1b[0m';
         }
         else if(level == 'read')
         {
-            color = '[' + prefix + '] \x0b[36m%s\x1b[0m', "[READ]";
+            color = '[' + prefix + '] \x0b[36m%s\x1b[0m';
         }
         else if(level == 'info')
         {
-            color = '[' + prefix + '] \x1b[33m%s\x1b[0m', "[INFO]";
+            color = '[' + prefix + '] \x1b[33m%s\x1b[0m';
         }
         else if(level == 'warn')
         {
-            color = '[' + prefix + '] \x0b[33m%s\x1b[0m', "[WARN]";
+            color = '[' + prefix + '] \x0b[33m%s\x1b[0m';
         }
         else
         {
-            color = '[' + prefix + '] \x1b[31m%s\x1b[0m', "[ERROR]";
+            color = '[' + prefix + '] \x1b[31m%s\x1b[0m';
         }
 
         var d = new Date();
