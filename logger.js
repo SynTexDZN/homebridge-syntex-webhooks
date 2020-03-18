9e980187492205ed5e59acb17edac71b18ceb539
SynTexDZN <syntexdzn@t-online.de>
2020-03-18 13:12:15 +0100

    fixes

--- logger.js
+++ logger.js
@@ -1,11 +1,16 @@
-var logger = exports;
-logger.debugLevel = 'error';
+var logger = exports, prefix;
+logger.debugLevel = 'success';
+
+logger.create = function(pluginName)
+{
+    prefix = pluginName;
+};
 
 logger.log = function(level, message)
 {
     var levels = ['success', 'update', 'read', 'info', 'warn', 'error'];
 
-    if(levels.indexOf(level) <= levels.indexOf(logger.debugLevel))
+    if(levels.indexOf(level) >= levels.indexOf(logger.debugLevel))
     {
         if(typeof message !== 'string')
         {
@@ -14,27 +19,27 @@ logger.log = function(level, message)
 
         if(level == 'success')
         {
-            console.log('\x1b[32m%s\x1b[0m', "[SUCCESS]", message);
+            console.log(prefix, '\x1b[32m%s\x1b[0m', "[SUCCESS]", message);
         }
         else if(level == 'update')
         {
-            console.log('\x1b[36m%s\x1b[0m', "[UPDATE]", message);
+            console.log(prefix, '\x1b[36m%s\x1b[0m', "[UPDATE]", message);
         }
         else if(level == 'read')
         {
-            console.log('\x0b[36m%s\x1b[0m', "[READ]", message);
+            console.log(prefix, '\x0b[36m%s\x1b[0m', "[READ]", message);
         }
         else if(level == 'info')
         {
-            console.log('\x1b[33m%s\x1b[0m', "[INFO]", message);
+            console.log(prefix, '\x1b[33m%s\x1b[0m', "[INFO]", message);
         }
         else if(level == 'warn')
         {
-            console.log('\x0b[33m%s\x1b[0m', "[WARN]", message);
+            console.log(prefix, '\x0b[33m%s\x1b[0m', "[WARN]", message);
         }
         else
         {
-            console.log('\x1b[31m%s\x1b[0m', "[ERROR]", message);
+            console.log(prefix, '\x1b[31m%s\x1b[0m', "[ERROR]", message);
         }
     }
 }
