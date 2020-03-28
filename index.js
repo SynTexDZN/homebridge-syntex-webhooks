1a28c3d50ef3554af160c76416706ada54f2f95d
SynTexDZN <syntexdzn@t-online.de>
2020-03-28 21:24:21 +0100

    clean ups and fixes

--- index.js
+++ index.js
@@ -622,7 +622,7 @@ SynTexWebHookStripeRGBAccessory.prototype.getHue = function(callback)
             else
             {
                 logger.log('info', 'CALLBACK: res.split');
-                callback(null, (res.split(':')[1] || 0));
+                callback(null, (getHSL(res.split(':')[1], res.split(':')[2], res.split(':')[3])[0] || 0));
             }
         });
     }
@@ -649,7 +649,7 @@ SynTexWebHookStripeRGBAccessory.prototype.getSaturation = function(callback)
             }
             else
             {
-                callback(null, (res.split(':')[2] || 100));
+                callback(null, (getHSL(res.split(':')[1], res.split(':')[2], res.split(':')[3])[1] || 100));
             }
         });
     }
@@ -676,7 +676,7 @@ SynTexWebHookStripeRGBAccessory.prototype.getBrightness = function(callback)
             }
             else
             {
-                callback(null, (res.split(':')[3] || 50));
+                callback(null, (getHSL(res.split(':')[1], res.split(':')[2], res.split(':')[3])[2] || 50));
             }
         });
     }
@@ -753,6 +753,37 @@ SynTexWebHookStatelessSwitchAccessory.prototype.getServices = function()
 {
     return this.service;
 };
+
+function getHSL(r, g, b)
+{
+    r /= 255, g /= 255, b /= 255;
+
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if(max == min)
+    {
+        h = s = 0;
+    }
+    else
+    {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+        switch(max)
+        {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+
+        h /= 6;
+    }
+
+    logger.log(h + "-" + s + "-" + l);
+
+    return [h, s, l];
+}
 
 function setRGB(url, hue, saturation, brightness)
 {
