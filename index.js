3e752e44944c03807541a3f848992caaa9158c2a
SynTexDZN <syntexdzn@t-online.de>
2020-05-27 16:49:41 +0200

    switch improvements

--- index.js
+++ index.js
@@ -585,47 +585,47 @@ function getHSL(state)
 
 function setRGB(accessory)
 {
+    var h = accessory.hue, s = accessory.saturation * 2, l = accessory.power ? accessory.brightness / 4 : 0;
+    var r = 0, g = 0, b = 0;
+
+    s /= 100;
+    l /= 100;
+
+    let c = (1 - Math.abs(2 * l - 1)) * s,
+        x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+        m = l - c/2;
+
+    if(0 <= h && h < 60)
+    {
+        r = c; g = x; b = 0;
+    }
+    else if(60 <= h && h < 120)
+    {
+        r = x; g = c; b = 0;
+    }
+    else if(120 <= h && h < 180)
+    {
+        r = 0; g = c; b = x;
+    }
+    else if(180 <= h && h < 240)
+    {
+        r = 0; g = x; b = c;
+    }
+    else if(240 <= h && h < 300)
+    {
+        r = x; g = 0; b = c;
+    }
+    else if(300 <= h && h < 360)
+    {
+        r = c; g = 0; b = x;
+    }
+
+    r = Math.round((r + m) * 255);
+    g = Math.round((g + m) * 255);
+    b = Math.round((b + m) * 255);
+
     if(accessory.power + ':' + r + ':' + g + ':' + b != accessory.value)
     {
-        var h = accessory.hue, s = accessory.saturation * 2, l = accessory.power ? accessory.brightness / 4 : 0;
-        var r = 0, g = 0, b = 0;
-
-        s /= 100;
-        l /= 100;
-
-        let c = (1 - Math.abs(2 * l - 1)) * s,
-            x = c * (1 - Math.abs((h / 60) % 2 - 1)),
-            m = l - c/2;
-
-        if(0 <= h && h < 60)
-        {
-            r = c; g = x; b = 0;
-        }
-        else if(60 <= h && h < 120)
-        {
-            r = x; g = c; b = 0;
-        }
-        else if(120 <= h && h < 180)
-        {
-            r = 0; g = c; b = x;
-        }
-        else if(180 <= h && h < 240)
-        {
-            r = 0; g = x; b = c;
-        }
-        else if(240 <= h && h < 300)
-        {
-            r = x; g = 0; b = c;
-        }
-        else if(300 <= h && h < 360)
-        {
-            r = c; g = 0; b = x;
-        }
-
-        r = Math.round((r + m) * 255);
-        g = Math.round((g + m) * 255);
-        b = Math.round((b + m) * 255);
-
         logger.log('update', "HomeKit Status für '" + accessory.name + "' geändert zu '" + accessory.power + ':' + r + ':' + g + ':' + b + "' ( " + accessory.mac + " )");
 
         if(accessory.url != '')
