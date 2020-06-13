2001f80888526d7725c3eea17faffb9362c005e0
SynTexDZN <syntexdzn@t-online.de>
2020-06-14 00:44:17 +0200

    improvements

--- index.js
+++ index.js
@@ -698,7 +698,7 @@ function createAccessory(accessory)
 
                 for(var j = 0; j < accessory.service.length; j++)
                 {
-                    logger.log('debug', accessory.service[j].character + ' - ' + accessory.service[j]);
+                    logger.log('debug', accessory.service[j]);
                     
                     if(accessory.type != 'rgb' && (type == null || type == accessory.service[j].type))
                     {
