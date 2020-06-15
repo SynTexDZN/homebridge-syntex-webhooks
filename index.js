528a2789f6595039a2232576eafde8956e847e55
SynTexDZN <syntexdzn@t-online.de>
2020-06-16 00:36:46 +0200

    added support for duplicate device types

--- index.js
+++ index.js
@@ -608,7 +608,7 @@ function SynTexBaseAccessory(accessoryConfig)
     {
         if(this.type.includes(accessories[i].type))
         {
-            var count = (this.type.match(new RegExp(accessories[i].type, 'g')) || []).length;
+            var count = (JSON.stringify(this.type).match(new RegExp(accessories[i].type, 'g')) || []).length;
             var characteristic = accessories[i].characteristic;
             var name = this.name;
 
