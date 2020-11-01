e6370e018ca8f7f37131d08b40a9163e85925e27
SynTex <syntexdzn@t-online.de>
2020-11-01 14:19:42 +0100

    Fixes

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ SynTexWebHookPlatform.prototype = {
                                 if(accessories[i].service[j].mac != null && accessories[i].service[j].letters != null)
                                 {
                                     console.log(accessories[i].service[j].letters[0], TypeManager.typeToLetter(urlParams.type));
-                                    console.log(urlParams.type == null, accessories[i].service[j].letters[0], TypeManager.typeToLetter(urlParams.type), urlParams.counter == null, accessories[i].service[j].letters[1], urlParams.counter));
+                                    console.log(urlParams.type == null, accessories[i].service[j].letters[0], TypeManager.typeToLetter(urlParams.type), urlParams.counter == null, accessories[i].service[j].letters[1], urlParams.counter);
 
                                     if((urlParams.type == null || accessories[i].service[j].letters[0] == TypeManager.typeToLetter(urlParams.type)) && (urlParams.counter == null || accessories[i].service[j].letters[1] == urlParams.counter))
                                     {
